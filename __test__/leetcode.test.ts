025fce8c671f6f02ac149b4158376bdc08ceffa1
chenyiyin <332990332@qq.com>
2019-03-15 23:54:47 +0800

    feat: add problem

--- __test__/leetcode.test.ts
+++ __test__/leetcode.test.ts
@@ -1,9 +1,62 @@
-import { reverse_integer } from '../leetcode';
+import { reverse_integer, palindrome_number, roman_to_integer, longest_common_prefix, valid_parentheses, remove_duplicates_from_sorted_array, remove_element, search_insert_position } from '../leetcode';
 describe('刷 leetcode', () => {
   test('整数反转', () => {
     expect(reverse_integer(123)).toBe(321);
     expect(reverse_integer(-123)).toBe(-321);
     expect(reverse_integer(120)).toBe(21);
     expect(reverse_integer(-120)).toBe(-21);
+  })
+  test('回文数', () => {
+    expect(palindrome_number(121)).toBe(true);
+    expect(palindrome_number(-121)).toBe(false);
+    expect(palindrome_number(10)).toBe(false);
+    expect(palindrome_number(11)).toBe(true);
+  })
+  test('罗马数字转整数', () => {
+    expect(roman_to_integer('III')).toBe(3);
+    expect(roman_to_integer('IV')).toBe(4);
+    expect(roman_to_integer('IX')).toBe(9);
+    expect(roman_to_integer('LVIII')).toBe(58);
+    expect(roman_to_integer('MCMXCIV')).toBe(1994);
+  })
+  test('最长公共前缀', () => {
+    expect(longest_common_prefix(["flower","flow","flight"])).toBe('fl')
+    expect(longest_common_prefix(["dog","racecar","car"])).toBe('')
+  })
+  test('判断是否正确的闭合了括号', () => {
+    expect(valid_parentheses('()')).toBe(true);
+    expect(valid_parentheses('()[]{}')).toBe(true);
+    expect(valid_parentheses('(]')).toBe(false);
+    expect(valid_parentheses('([)]')).toBe(false);
+    expect(valid_parentheses('{[]}')).toBe(true);
+  })
+  test('给排序过的数组去重并返回去重后数组的长度', () => {
+    const arr1 = [1, 1, 2]
+    const arr2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    expect(remove_duplicates_from_sorted_array(arr1)).toBe(2);
+    expect(arr1).toEqual(
+      [1, 2]
+    )
+    expect(remove_duplicates_from_sorted_array(arr2)).toBe(5);
+    expect(arr2).toEqual(
+      [0, 1, 2, 3, 4]
+    )
+  })
+  test('在原数组中移除输入的项', () => {
+    const arr1 = [3, 2, 2, 3]
+    const arr2 = [0, 1, 2, 2, 3, 0, 4, 2]
+    expect(remove_element(arr1, 3)).toBe(2);
+    expect(arr1).toEqual([2, 2])
+    expect(remove_element(arr2, 2)).toBe(5)
+    expect(arr2).toEqual(
+      [0, 1, 3, 0, 4]
+    )
+  })
+  test('搜索有序数组的插入位置', () => {
+    const arr = [1, 3, 5, 6]
+    expect(search_insert_position(arr, 5)).toBe(2);
+    expect(search_insert_position(arr, 2)).toBe(1);
+    expect(search_insert_position(arr, 7)).toBe(4);
+    expect(search_insert_position(arr, 0)).toBe(0);
   })
 })
--- leetcode.ts
+++ leetcode.ts
@@ -11,3 +11,142 @@ export function reverse_integer(x:number) : number {
   }
   return result
 }
+
+// 判断一个数是不是回文数
+export function palindrome_number(x:number) : boolean {
+  // 最后一位是0且不是0本身的话, 倒过来会缺少一位0 所以肯定不是回文数
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
+  }
+  // 获取 x 的后半段数字
+  let x_half_last = 0;
+  while (x > x_half_last) {
+    x_half_last = x_half_last * 10 + x % 10;
+    x = Math.floor(x / 10);
+  }
+  
+  /**
+   * * 当x的位数为奇数时, 根据上述的判断会给 x_half_last 多判一个中位数
+   * * 但此中位数是不影响结果的, 所以我们可以把它去掉
+   */
+  return x === x_half_last || x === Math.floor(x_half_last / 10);
+}
+
+// 罗马数字转整数
+export function roman_to_integer(s:string) : number {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let num = 0;
+  while (s.length) {
+    // 如果当前字符在 map 中比下一个字符小, 则认为他们代表一个数
+    const current_char = s.charAt(0);
+    const next_char = s.charAt(1);
+    if (next_char && map[current_char] < map[next_char]) {
+      num += map[next_char] - map[current_char];
+      s = s.substring(2);
+    } else {
+      num += map[current_char];
+      s = s.substring(1);
+    }
+  }
+  return num;
+}
+
+// 最长公共前缀
+export function longest_common_prefix(strs:string[]) : string {
+  let result = ''
+  if (strs.length === 0) {
+    return ''
+  }
+  if (strs.length === 1) {
+    return strs[0]
+  }
+  for (let i = 0; i < strs[0].length; i++) {
+    let current_char = strs[0].charAt(i);
+    let is_same = true
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j].charAt(i) !== current_char) {
+        is_same = false
+        break
+      }
+    }
+    if (is_same) {
+      result += current_char
+    } else {
+      break;
+    }
+  }
+  return result
+}
+
+// 判断大中小括号是否正确的跟最邻近的对应括号匹配, 此字符串中只含有这些括号
+export function valid_parentheses(s:string) : boolean {
+  if (!s.length) {
+    return true;
+  }
+  let reg = /\(\)|\[\]|\{\}/g;
+  while(reg.test(s)) {
+    s = s.replace(reg, '');
+  }
+  return s.length === 0;
+}
+
+// 去除一个已经排过序的数组的重复项
+export function remove_duplicates_from_sorted_array(nums:number[]) : number {
+  if (nums.length < 2) {
+    return nums.length
+  }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  
+  return nums.length
+}
+
+export function remove_element(nums:number[], val:number) : number {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
+
+export function search_insert_position(nums:number[], target:number) : number {
+  if (target > nums[nums.length - 1]) {
+    return nums.length;
+  }
+  let index = 0;
+  function split_arr_and_check(arr:number[], target) {
+    if (arr.length === 1) {
+      return;
+    }
+    const middle_num = Math.floor(arr.length / 2);
+    const left_arr = arr.slice(0, middle_num);
+    const right_arr = arr.slice(middle_num)
+    
+    console.log(left_arr);
+    console.log(right_arr);
+    
+    
+    if (target < right_arr[0]) {
+      split_arr_and_check(left_arr, target);
+    } else {
+      split_arr_and_check(right_arr, target);
+      index += middle_num;
+    }
+  }
+  split_arr_and_check(nums, target)
+  return index
+}
